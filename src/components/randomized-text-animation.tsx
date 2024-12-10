1d2aa237b70a9b02cf776c13d8e85a8f125eeb65
Dharma Yudistira <dharmayudistira2000@gmail.com>
2024-12-10 20:56:27 +0700

    add randomized text

---
+++ src/components/randomized-text-animation.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { generateRandomText } from "@/utils/randomizeText";
+import { useEffect, useState } from "react";
+
+interface RandomItem {
+    id: number;
+    text: string;
+    top: string;
+    left: string;
+};
+
+export function RandomizedTextAnimation() {
+
+    const [items, setItems] = useState<RandomItem[]>([]);
+
+    const getRandomPosition = () => ({
+        top: `${Math.floor(Math.random() * 90)}%`, // Avoid extreme edges
+        left: `${Math.floor(Math.random() * 90)}%`,
+    });
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setItems((prevItems) => {
+                const newItem: RandomItem = {
+                    id: Math.random(),
+                    text: generateRandomText(),
+                    ...getRandomPosition(),
+                };
+
+                // Maintain only 5 items at a time
+                return prevItems.length < 10
+                    ? [...prevItems, newItem]
+                    : [...prevItems.slice(1), newItem];
+            });
+        }, 1000); // Update every 1 second
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <>
+            {items.map((item) => (
+                <div
+                    key={item.id}
+                    data-aos="fade-up"
+                    className="absolute text-lg font-bold text-white animate-fade"
+                    style={{
+                        top: item.top,
+                        left: item.left,
+                        transform: 'translate(-50%, -50%)',
+                    }}
+                >
+                    {item.text}
+                </div>
+            ))}
+        </>
+    )
+}
