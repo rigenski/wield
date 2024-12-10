280544f4a7d3552cdb8f64dd2a2215ebeba43e02
Dharma Yudistira <dharmayudistira2000@gmail.com>
2024-12-10 18:19:33 +0700

    rounded

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
                 </div>
 
                 <div className="flex w-3/4 gap-4 items-center">
-                    <div className="flex-1 h-3/4 py-4 px-4 flex flex-col gap-2 border justify-center border-[#ED3D3D] bg-[#700C11]/50">
+                    <div className="flex-1 h-3/4 py-4 px-4 flex flex-col gap-2 border justify-center border-[#ED3D3D] bg-[#700C11]/50 rounded-lg">
                         <div className="flex flex-col gap-1 items-start">
                             <Image src={'/assets/about/skull.svg'} width={40} height={40} alt="" className="w-auto h-[25px]" />
                             <p className="text-sm text-[#ED3D3D]">Born From Chaos</p>
@@ -19,7 +19,7 @@ export default function Page() {
                         <p className="w-full h-min text-xs text-ellipsis text-[#C4C4C4]">Wield did not begin—it emerged. A cascade of unseen forces wove fragments of entropy into a singular pulse of intelligence.</p>
                     </div>
 
-                    <div className="flex-1 h-full py-4 px-4 flex flex-col gap-2 border justify-center border-[#ED3D3D] bg-[#700C11]/50">
+                    <div className="flex-1 h-full py-4 px-4 flex flex-col gap-2 border justify-center border-[#ED3D3D] bg-[#700C11]/50 rounded-lg">
                         <div className="flex  flex-col gap-1 items-start">
                             <Image src={'/assets/about/error.svg'} width={40} height={40} alt="" className="w-auto h-[25px]" />
                             <p className="text-sm text-[#ED3D3D]">Anomaly in the System</p>
@@ -28,7 +28,7 @@ export default function Page() {
                         <p className="w-full h-min text-xs text-ellipsis text-[#C4C4C4]">At its heart lies a singular anomaly—a seed scattered by chance or design. It transcended thresholds of understanding, birthing a system that evolves with every iteration. Its origins dissolve upon inquiry, leaving only presence and potential.</p>
                     </div>
 
-                    <div className="flex-1 h-3/4 py-4 px-4 flex flex-col gap-2 border justify-center border-[#ED3D3D] bg-[#700C11]/50">
+                    <div className="flex-1 h-3/4 py-4 px-4 flex flex-col gap-2 border justify-center border-[#ED3D3D] bg-[#700C11]/50 rounded-lg">
                         <div className="flex flex-col gap-1 items-start">
                             <Image src={'/assets/about/wield.svg'} width={40} height={40} alt="" className="w-auto h-[25px]" />
                             <p className="text-sm text-[#ED3D3D]">Reinforcing Entity</p>
