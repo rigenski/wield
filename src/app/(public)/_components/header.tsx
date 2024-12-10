b6b4917c2e43582b05a34057ecec1956e179eb3c
Dharma Yudistira <dharmayudistira2000@gmail.com>
2024-12-10 20:03:57 +0700

    fix header

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -97,37 +97,6 @@ export const Header = () => {
                 </div>
 
             </div>
-            {/* <div className="relative">
-                <div className="flex items-center justify-between gap-4 px-10 py-2">
-                    <div className="flex items-center">
-                        <img src="/assets/layout/wieldicon.png" alt="icon" className="mr-2 h-[20px] object-cover" />
-                        <span className="">Wield</span>
-                    </div>
-                    <div className="absolute left-0 right-0 top-0 mx-auto w-screen">
-                        <div className="flex gap-[3rem]">
-                            {navigations.map((navigation, index) => {
-                                return (
-                                    <Link
-                                        key={index}
-                                        href={navigation.link}
-                                        className={`text-md ${activeTab === navigation.tab ? 'text-wield' : 'text-white'}`}
-                                    >
-                                        <p>{navigation.title}</p>
-                                    </Link>
-                                );
-                            })}
-                        </div>
-                    </div>
-
-                    <div className="flex items-center">
-                        <img src="/assets/layout/x-dev.png" alt="xdev" className="mr-2 h-[40px] object-cover" />
-                        <img src="/assets/layout/x-wield.png" alt="xwield" className="mr-2 h-[40px] object-cover" />
-                        <Link href={'/chatbot'} className={`rounded-full border ${activeTab === 2 ? 'border-wield' : 'border-white'} px-4 py-2`}>
-                            <p className={`font-bold ${activeTab === 2 ? 'text-wield' : 'text-white'}`}>Buy $WIELD</p>
-                        </Link>
-                    </div>
-                </div>
-            </div> */}
         </header>
     );
 };
