299fe5e0aeb17b76c588871fa8c1ab5f42b5c868
Rigen Maulana <rygen.dev@gmail.com>
2024-12-10 21:02:18 +0700

    fix

--- src/app/(public)/_components/section-1.tsx
+++ src/app/(public)/_components/section-1.tsx
@@ -1,26 +1,29 @@
 'use client';
 
-import { motion, useScroll, useTransform } from 'framer-motion';
-
 export function Section1() {
-    const { scrollY } = useScroll();
-    const opacity = useTransform(scrollY, [0, 300], [1, 0]);
+
 
     return (
-        <section className="relative h-screen w-full overflow-hidden">
-            <div className="absolute left-0 right-0 top-0 z-0 h-full w-full">
-                <div className="h-screen w-full bg-gradient-to-b from-[#ED3D3D]/50 to-20%" />
-            </div>
+        <div className="relative w-full">
+            <section className="relative h-screen w-full overflow-hidden" style={{ height: '200vh' }}>
+                <div className="flex h-full w-full flex-col justify-between">
+                    <div className="h-full w-full">
+                        <div className="h-screen w-full bg-gradient-to-b from-[#ED3D3D]/50 to-20%" />
+                    </div>
 
-            <div className="absolute bottom-0 left-0 right-0 z-0 h-full w-full">
-                <div className="h-screen w-full bg-gradient-to-t from-[#ED3D3D]/50 to-20%" />
-            </div>
+                    <div className="h-full w-full">
+                        <div className="h-screen w-full bg-gradient-to-t from-[#ED3D3D]/50 to-20%" />
+                    </div>
+                </div>
+            </section>
 
-            <div className="absolute flex size-full flex-col items-center justify-center">
-                <motion.p className="mb-24 text-[140px] font-bold text-[#FFE0E0] [text-shadow:0px_0px_5px_#fff]" style={{ opacity }}>
+            <div className="absolute left-1/2 top-1/4 z-50">
+                <div
+                    className="-ml-48 mb-24 text-[140px] font-bold text-[#FFE0E0] [text-shadow:0px_0px_5px_#fff]"
+                >
                     Wield
-                </motion.p>
+                </div>
             </div>
-        </section>
+        </div>
     );
 }
