a0b1e73668b065128b7bc09e02ba8bcc0fa63705
Dhimas.Pamungkas [ITB-SOE] <P241123003@bussan.co.id>
2024-12-10 19:48:52 +0700

    feat: arc

--- src/app/(public)/architecture/components/list-tile.tsx
+++ src/app/(public)/architecture/components/list-tile.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const ListTile = ({ title, description }: { title: string, description: string }) => {
     return (
         <div
-            className="px-4 py-10 border-[#ED3D3D] border-2 w-[60vw] text-[1.5rem] mb-10 z-20"
+            className="px-4 py-12 border-[#ED3D3D] border-2 w-[60vw] text-[1.5rem] mb-10 z-20"
             style={{
-                background: "rgba(255, 255, 255, 0.1)",
-                boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+                background: "rgba(255, 255, 255, 0.01)",
+                boxShadow: '0 4px 30px rgba(0, 0, 0, 0.01)',
                 backdropFilter: 'blur(5px)',
                 WebkitBackdropFilter: 'blur(5px)',
             }}
@@ -14,8 +14,8 @@ const ListTile = ({ title, description }: { title: string, description: string }
             data-aos-delay="100"
             data-aos-once="true"
         >
-            <p className='text-[#ED3D3D] [text-shadow:0px_0px_5px_#ED3D3D] gilroy-bold'>{title}</p>
-            <p>{description}</p>
+            <p className='text-lg text-[#ED3D3D] [text-shadow:0px_0px_5px_#ED3D3D] gilroy-bold mb-2'>{title}</p>
+            <p className='text-sm text-[#B5B0B0]'>{description}</p>
         </div>
     )
 }
--- src/app/(public)/architecture/page.tsx
+++ src/app/(public)/architecture/page.tsx
@@ -4,16 +4,16 @@ import ListTile from './components/list-tile';
 
 const Page = () => {
     return (
-        <div className="flex flex-row">  {/* Removed justify-between */}
+        <div className="flex flex-row"> 
             <img className="fixed top-0 left-0 w-screen object-fill z-0" src='assets/layout/overlay.png' />
-            <div className="mx-10 mt-[100px]">  {/* Added overflow-y-auto and height */}
+            <div className="mx-10 mt-[100px]">  
                 <h1 className='text-[3rem] [text-shadow:0px_0px_5px_#fff] my-5 gilroy-bold'>Core Architecture</h1>
                 {coreData.map(data => (<ListTile title={data.title} description={data.text} />))}
             </div>
-            <div className="relative w-[50vw] h-full">
+            <div className="relative h-full">
                 <video
                     src="assets/architecture/architecture.mp4"
-                    className='object-fill'
+                    className='w-[40vw] object-fill'
                     autoPlay
                     muted
                     loop
